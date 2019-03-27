e7c6c77189b5ab7bee9c1c6d38e9d9ad76bc3372
Merline Belizaire <merline@noukod.com>
2019-03-27 17:05:56 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,7 +3,7 @@ class RepositoriesController < ApplicationController
   def search
 
   end
-begin
+
   def github_search
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['client_id'] = "a36571b8ecd826c18b1c"
