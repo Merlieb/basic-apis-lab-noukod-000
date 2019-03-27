c9fd67a160cf40a2a5d2552f67eda93e25a677e5
Merline Belizaire <merline@noukod.com>
2019-03-27 17:04:47 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,3 +1,4 @@
+
 class RepositoriesController < ApplicationController
 
   def search
@@ -5,13 +6,17 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
-      req.params['client_id'] = "a36571b8ecd826c18b1c"
-      req.params['client_secret'] = "2270fd114d170200b1e32a8e40579b983562de42"
-      req.params['query'] = "malouApp"
+    client_id = "REPLACE ME"
+    secret = "REPLACE ME"
+
+    resp = Faraday.get("https://api.github.com/search/repositories") do |req|
+      req.params["q"] = params[:query]
+      req.params["client_id"] = client_id
+      req.params["client_secret"] = secret
     end
-    @body_hash=JSON.parse(@resp.body)
-    puts @body_hash.inspect
-    render "search"
+
+    body = JSON.parse(resp.body)
+    @results = body["items"]
+    render :search
   end
 end
